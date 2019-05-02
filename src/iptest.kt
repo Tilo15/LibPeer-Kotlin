40dcf45d7e103115b0212dbfb8024568a2f24032
Billy Barrow <billyb@pcthingz.com>
2019-05-02 18:11:10 +1200

    Implemented deserialisation of BinaryAddress

--- src/iptest.kt
+++ src/iptest.kt
@@ -16,6 +16,15 @@ fun main() {
     // Listen for data
     network.incoming.subscribe {
         println("Message from ${it.address}: ${it.data.toString(UTF_8)}")
+
+        // Print hex of binary address
+        for (b in it.address.serialise()) {
+            val st = String.format("%02X", b)
+            print(st)
+        }
+        print("\n")
+
+        println(BinaryAddress.deserialise(it.address.serialise()).toString())
     }
 
     while (true) {
--- src/libpeer/formats/BinaryAddress.kt
+++ src/libpeer/formats/BinaryAddress.kt
@@ -1,5 +1,6 @@
 package libpeer.formats
 
+import libpeer.util.DeserialisationSegment
 import kotlin.text.Charsets.UTF_8
 
 class BinaryAddress(
@@ -9,6 +10,13 @@ class BinaryAddress(
     val application: ByteArray = ByteArray(0),
     val label: ByteArray = ByteArray(0)
 ) {
+
+    init {
+        // Error if invalid label
+        if(label.isNotEmpty() && label.size != 32) {
+            throw Exception("Binary address label must either be of length 0, or 32")
+        }
+    }
 
     override fun toString(): String {
         // Start the formatting
@@ -33,8 +41,116 @@ class BinaryAddress(
     }
 
     fun serialise(): ByteArray {
-        // TODO
-        return ByteArray(0)
+
+        // Calculate buffer size to allocate
+        var size = application.size + label.size + networkType.size + networkAddress.size + networkPort.size + 5
+
+
+        // Create buffer
+        val buffer = ByteArray(size)
+
+        // Keep track of where we are in the buffer
+        var position = 0
+
+        buffer[position] = 0x01
+        position ++
+
+        application.copyInto(buffer, position)
+        position += application.size
+
+        if(label.isEmpty()){
+            buffer[position] = 0x02
+            position ++
+        }
+        else{
+            buffer[position] = 0x2F
+            position ++
+
+            label.copyInto(buffer, position)
+            position += label.size
+        }
+
+        networkType.copyInto(buffer, position)
+        position += networkType.size
+
+        buffer[position] = 0x1F
+        position ++
+
+        networkAddress.copyInto(buffer, position)
+        position += networkAddress.size
+
+        buffer[position] = 0x1F
+        position ++
+
+        networkPort.copyInto(buffer, position)
+        position += networkPort.size
+
+        buffer[position] = 0x04
+
+        // Return the buffer
+        return buffer
+
+    }
+
+    companion object {
+        fun deserialise(buffer: ByteArray): BinaryAddress {
+
+            if(buffer[0] != 0x01.toByte()){
+                throw IllegalArgumentException("Passed ByteArray does not represent a valid BinaryAddress")
+            }
+
+            val labelDelimiter = buffer.indexOf(0x2F)
+            val networkDelimiter = buffer.indexOf(0x02)
+
+            val hasLabel = labelDelimiter != -1
+
+            val delimiter = if(hasLabel) labelDelimiter else networkDelimiter
+
+            // Get the application namespace
+            val application = ByteArray(delimiter -1)
+            buffer.copyInto(application, 0, 1, delimiter)
+
+            // Calculate the position to start at
+            var position = application.size + 2
+
+            // Somewhere to hold the label
+            var label: ByteArray
+
+            // Does it have a label?
+            if(hasLabel) {
+                // Get ready to hold the label
+                label = ByteArray(32)
+
+                // Copy into label
+                buffer.copyInto(label, 0, position, position + 32)
+
+                // Increase position
+                position += 32
+            }
+            else {
+                // Zero length label
+                label = ByteArray(0)
+            }
+
+            // Read network field
+            val networkSegment = DeserialisationSegment(buffer, 0x1F, position)
+
+            // Read address field
+            val addressSegment = networkSegment.nextSegement(0x1F)
+
+            // Read the port field
+            val portSegment = addressSegment.nextSegement(0x04)
+
+            // Return a new binary address with this data
+            return BinaryAddress(
+                networkSegment.segment,
+                addressSegment.segment,
+                portSegment.segment,
+                application,
+                label
+            )
+
+        }
     }
 
 }
